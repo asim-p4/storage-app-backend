f301e05002f28300dcc6be752a50bdebe2c43ef1
asim-p4 <asim00silent99@gmail.com>
2026-05-18 06:39:55 +0000

    remove comment in app.js

--- app.js
+++ app.js
@@ -42,7 +42,6 @@ app.use("/webhooks", webhook);
 
 app.use((err, req, res, next) => {
   console.log(err);
-  // res.status(err.status || 500).json({ error: "Something went wrong!" });
   res.json(err);
 });
 
