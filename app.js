701b5dd4afad24326137e8dcccb437cba3bb73ca
asim-p4 <asim00silent99@gmail.com>
2026-06-07 11:24:22 +0500

    add backend ci/cd

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "in progress" });
+  res.status(200).json({ message: "progress" });
 });
 
 app.post("/github-webhook", (req, res) => {
