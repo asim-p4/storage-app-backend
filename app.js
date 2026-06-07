540d7f8ba0f6f8d98db5499c0b58120678d49726
asim-p4 <asim00silent99@gmail.com>
2026-06-07 13:22:53 +0500

    chnage check message

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "d" });
+  res.status(200).json({ message: "dhhhhhh" });
 });
 
 app.post("/github-webhook", (req, res) => {
