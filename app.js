206a6eb06cb6e459ac3a5c7b77ac6c0d19e2992e
asim-p4 <asim00silent99@gmail.com>
2026-06-07 11:41:18 +0500

    add backend ci/cd

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import 'dotenv/config'
 
 import directoryRoutes from "./routes/directoryRoutes.js";
 import fileRoutes from "./routes/fileRoutes.js";
