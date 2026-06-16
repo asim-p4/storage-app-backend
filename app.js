82b1fe174685ec91a00a5a0e6bab9c1e2f2ca51b
asim-p4 <asim00silent99@gmail.com>
2026-06-16 15:10:33 +0500

    server main

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "ljjjb" });
+  res.status(200).json({ message: "ljb" });
 });
 
 app.use("/directory", checkAuth, directoryRoutes);
