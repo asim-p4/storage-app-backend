07b65b4544c4f930d7a3d93c82bb7006995f04ae
asim-p4 <asim00silent99@gmail.com>
2026-06-17 00:10:06 +0500

    server testing

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "wertyuiop" });
+  res.status(200).json({ message: "yuiop" });
 });
 
 app.use("/directory", checkAuth, directoryRoutes);
