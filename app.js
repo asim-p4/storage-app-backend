11ef8a946ab67eb3f1600d6d13adee62ed3f4637
asim-p4 <asim00silent99@gmail.com>
2026-06-19 11:16:20 +0500

    server main

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "fifth version!", timestamp: new Date().toISOString() });
+  res.status(200).json({ status: "fifth version!!", timestamp: new Date().toISOString() });
 });
 
 app.use("/directory", checkAuth, directoryRoutes);
